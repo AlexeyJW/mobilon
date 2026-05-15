306044cf9df3c90dc50feb6625d4b3f0d7dee17a
AlexeyJW <kononenko.jw@gmail.com>
2026-05-15 12:18:10 +0300

    theme dark-light

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,9 @@
 <!-- layouts/default.vue -->
 <template>
+   <ThemeToggle />
    <MobileHeader />
-  <div class="min-h-screen bg-gradient-to-br from-gray-50 via-white to-gray-100 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900">
-    
-    <slot />
+   <div class="min-h-screen bg-gradient-to-br from-gray-50 via-white to-gray-100 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900">
+     <slot />
   </div>
 </template>
 
