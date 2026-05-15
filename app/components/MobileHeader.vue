6ea957bac0a15d1b68dfbf2a426ccadd3c292844
AlexeyJW <kononenko.jw@gmail.com>
2026-05-15 11:12:50 +0300

    slideout, page about, contacts

--- app/components/MobileHeader.vue
+++ app/components/MobileHeader.vue
@@ -1,5 +1,27 @@
+ <script setup lang="ts">
+const open = ref(false)
+
+defineShortcuts({
+  o:() =>  open.value = !open.value
+
+})
+const route = useRoute()
+
+const links = [
+  { label: 'Головна', to: '/' },
+  { label: 'Про нас', to: '/about' },
+  { label: 'Контакти', to: '/contacts' },
+  { label: 'Адмін', to: '/admin/login' }
+]
+</script>
  <template>
-    <USlideover side="left" title="Mobilon">
+    <USlideover 
+        v-model:open="open"
+         :ui="{
+      content: 'w-1/2'
+    }"
+        side="left" 
+        title="Mobilon">
         <UButton 
         
          icon="i-heroicons-bars-3"
@@ -9,32 +31,19 @@
     <template #body>
       <div class="flex flex-col gap-4 mt-4">
         <NuxtLink
-          to="/"
-          @click="open = false"
-        >
-          Головна
-        </NuxtLink>
-
-        <NuxtLink
-          to="/about"
-          @click="open = false"
-        >
-          Про нас
-        </NuxtLink>
-
-        <a
-          href="#contact-form"
-          @click="open = false"
-        >
-          Контакти
-        </a>
-
-       <NuxtLink
-          to="/admin/login"
-          @click="open = false"
-        >
-          Адмін
-        </NuxtLink>
+      @click="open = false"
+      v-for="link in links"
+      :key="link.to"
+      :to="link.to"
+      class="px-4 py-2 rounded-lg transition"
+       :class="
+    route.path === link.to
+      ? 'bg-black/5 dark:bg-white/10'
+      : 'hover:bg-black/5 focus:bg-black/5 dark:hover:bg-white/10 dark:focus:bg-white/10'
+  "
+    >
+      {{ link.label }}
+    </NuxtLink>
       </div>
       
     </template>
