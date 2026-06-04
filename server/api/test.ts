da2464229b0b020d44c15bfc909ed6074d3751f1
AlexeyJW <kononenko.jw@gmail.com>
2026-06-04 14:12:07 +0300

    fixed deploy

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,15 +1,16 @@
 import prisma from '../utils/prisma'
 
 export default defineEventHandler(async () => {
-  try {
-    const requests = await prisma.request.findMany()
 
-    return requests
-  } catch (error) {
-    console.error('API TEST ERROR:', error)
-
-    return {
-      error: String(error)
+  const requests = await prisma.request.findMany({
+    include: {
+      customer: true
+    },
+    orderBy: {
+      createdAt: 'desc'
     }
-  }
+  })
+
+  return requests
+
 })
