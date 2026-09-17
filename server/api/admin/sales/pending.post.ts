42debe9083e711b3a99c32be6dd75fe4f75ac712
AlexeyJW <kononenko.jw@gmail.com>
2026-09-17 17:55:23 +0300

    fixed

--- server/api/admin/sales/pending.post.ts
+++ server/api/admin/sales/pending.post.ts
@@ -108,7 +108,7 @@ export default defineEventHandler(async (event) => {
 
   const policy = await prisma.bonusPolicy.findFirst({
     where: {
-      active: true,
+      
       validFrom: {
         lte: now
       },
